6d68dc1991295b2111f2f16f465ba8908159d240
mozAndrey <3402391995aZ>
2022-08-16 16:33:10 +0300

    initial commit

---
+++ bill-service/src/main/java/example/BillApplication.java
@@ -0,0 +1,13 @@
+package example;
+
+/**
+ * Hello world!
+ *
+ */
+public class BillApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
