d18af2f8a99dd7830aaf2dcf3049e874549c570c
mozAndrey <3402391995aZ>
2022-08-17 15:54:25 +0300

    Frth

--- config-service/src/main/java/org/example/ConfigApplication.java
+++ config-service/src/main/java/org/example/ConfigApplication.java
@@ -1,13 +1,13 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class ConfigApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
--- gateway/src/main/java/org/example/GatewayApplication.java
+++ gateway/src/main/java/org/example/GatewayApplication.java
@@ -1,13 +1,15 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class GatewayApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
